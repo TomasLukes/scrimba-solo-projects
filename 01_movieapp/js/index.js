cca0fd61544e3b1c7a91e386512fa54b136acb3a
TomasLukes <lukest@seznam.cz>
2023-04-30 20:45:13 +0200

    Refactored index.js (delete unused imports)

--- 01_movieapp/js/index.js
+++ 01_movieapp/js/index.js
@@ -1,9 +1,4 @@
-import { apiKey, myWatchlist, searchResultsContainer, htmlFeed } from './app.js';
-
-import { handleClick, searchMovies, getFeed, render, addToWatchlist, removeFromWatchlist } from './app.js';
-
-const searchInput = document.getElementById('searchInput');
-const searchBtn = document.getElementById('searchBtn');
+import { handleClick} from './app.js';
               
 // Add event listeners
 document.addEventListener('click', handleClick);
