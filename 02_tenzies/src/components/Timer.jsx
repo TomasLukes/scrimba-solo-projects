82cf3df012b79ab6a9ea7dac024c68916a5154d3
TomasLukes <lukest@seznam.cz>
2023-05-01 17:46:18 +0200

    Animated Timer code snippet implemented

--- 02_tenzies/src/components/Timer.jsx
+++ 02_tenzies/src/components/Timer.jsx
@@ -1,7 +1,25 @@
+import '../assets/styles/timer.css';
+
 export default function Timer({ time }) {
   return (
-    <div>
-      <h2>{time} seconds</h2>
+    <div className="count-up-timer" style={{ '--display-time': time }}>
+      {time}
     </div>
-  )
+  );
 }
+
+/*
+const CountUpTimer = ({ time }) => {
+  const [displayTime, setDisplayTime] = useState(0);
+
+  useEffect(() => {
+    setDisplayTime(time);
+  }, [time]);
+
+  return (
+    <div className="count-up-timer" style={{ '--display-time': displayTime }}>
+      {displayTime}
+    </div>
+  );
+};
+*/
