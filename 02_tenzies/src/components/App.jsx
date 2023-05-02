a2289003e99f23c0770be90f5db314fc075c4044
TomasLukes <lukest@seznam.cz>
2023-05-02 09:19:53 +0200

    Add CSS tooltip for info icon

--- 02_tenzies/src/components/App.jsx
+++ 02_tenzies/src/components/App.jsx
@@ -112,7 +112,6 @@ export default function App() {
           <div className='heading__description'>
                 <div className='flex'>
                   <h2 className='heading__description-title no-margin'>Welcome to <Logo /></h2>
-                  <svg className='heading__info-icon' height="48" viewBox="0 0 48 48" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h48v48h-48z" fill="none"/><path d="M22 34h4v-12h-4v12zm2-30c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z"/></svg>
                 </div>
             <p className="heading__description-text no-margin">
               Roll, hold, and match dice numbers. Click to hold a die, keep rolling to win!
