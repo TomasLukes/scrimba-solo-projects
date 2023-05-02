9d8d4f7dd23e5b0ba9649dc246b24eabba8f0e40
TomasLukes <lukest@seznam.cz>
2023-05-02 22:01:09 +0200

    Add media queries for basic responsivnes

--- 02_tenzies/src/components/App.jsx
+++ 02_tenzies/src/components/App.jsx
@@ -122,7 +122,7 @@ export default function App() {
           </div>
         }
 
-        {tenzies && (recordBeaten ? <p>You did it! You've set a new record!</p> : <p>Not quite there, give it another try!</p>)}
+        {tenzies && (recordBeaten ? <p>You did it! You've set a new record!"</p> : <p>Not quite there, give it another try!</p>)}
         {timeIsRunning ? <Timer time={time} /> : <Record bestRecord={bestRecord} />}
         
         <div className='dice'>
--- 02_tenzies/src/components/Record.jsx
+++ 02_tenzies/src/components/Record.jsx
@@ -1,7 +1,7 @@
 export default function Record({ bestRecord }) {
   return (
     <div className="record">
-      <p className="record__text no-margin">⏱️ Best record is: <span className="record__seconds">{bestRecord}</span> seconds</p>
+      <p className="record__text no-margin">⏱️ Best record: <span className="record__seconds">{bestRecord}</span> seconds</p>
     </div>
   )
 }
