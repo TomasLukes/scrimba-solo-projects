3bfade214decead0e2c694c73db03aab72d7a58c
TomasLukes <lukest@seznam.cz>
2023-05-03 18:28:20 +0200

    02/ Alert message when dice clicked before start

--- 02_tenzies/src/components/Die.jsx
+++ 02_tenzies/src/components/Die.jsx
@@ -1,6 +1,6 @@
 export default function Die({ value, timeIsRunning, isHeld, holdDice }) {
   return (
-    <div className={`die ${!timeIsRunning && 'die--not-started'} ${isHeld && 'die--isHeld'}`} onClick={holdDice}>
+    <div className={`die ${!timeIsRunning && 'die--not-started'} ${isHeld && 'die--isHeld'}`} onClick={timeIsRunning ? holdDice : () => alert("You must start game first")}>
       <h2 className="die__value no-margin">{value}</h2>
     </div>
   )
