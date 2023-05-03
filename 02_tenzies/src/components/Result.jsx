13ed37090f9facb67f1d8cfc36bda6cba229961d
TomasLukes <lukest@seznam.cz>
2023-05-03 18:10:12 +0200

    02/ Improved tooltip resposivity

--- 02_tenzies/src/components/Result.jsx
+++ 02_tenzies/src/components/Result.jsx
@@ -2,8 +2,8 @@ export default function Result({ recordBeaten }) {
   return (
     <div>
       {recordBeaten ?
-        <p>You did it! You've set a new record!</p> :
-        <p>Not quite there, give it another try!</p>
+        <p className="record__text">You did it! You've set a new record!</p> :
+        <p className="record__text">Not quite there, give it another try!</p>
       }
     </div>
   )
