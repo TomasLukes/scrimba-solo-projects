2c75e9359d064222dac938028194d4fe86d3efbc
TomasLukes <lukest@seznam.cz>
2023-05-03 18:14:59 +0200

    02/ Results styling + responsivity

--- 02_tenzies/src/components/Result.jsx
+++ 02_tenzies/src/components/Result.jsx
@@ -2,8 +2,8 @@ export default function Result({ recordBeaten }) {
   return (
     <div>
       {recordBeaten ?
-        <p className="record__text">You did it! You've set a new record!</p> :
-        <p className="record__text">Not quite there, give it another try!</p>
+        <p className="result__text">You did it! You've set a new record! 🔥</p> :
+        <p className="result__text">Not quite there, give it another try! 💪</p>
       }
     </div>
   )
