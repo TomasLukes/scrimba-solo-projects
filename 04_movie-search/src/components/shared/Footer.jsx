5c233c2abb62ef912a9534df7b47e3ba951800cd
TomasLukes <lukest@seznam.cz>
2023-05-07 10:58:23 +0200

    04/ Refactored BeforeSearch, Footer CSS

--- 04_movie-search/src/components/shared/Footer.jsx
+++ 04_movie-search/src/components/shared/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="flex">
-        <p className="text-author">©2023 by Tomáš Lukeš</p>
+    <footer className="footer">
+        <p className="footer__author">©2023 by <a href="https://tomaslukes.com">Tomáš Lukeš</a></p>
     </footer>
   )
 }
