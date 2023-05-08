a76a8397a8bd418ef789f4a3a709de421f5fb4bf
TomasLukes <lukest@seznam.cz>
2023-05-08 12:35:24 +0200

    04/ Test fix for path problem after deployment

--- 04_movie-search/src/components/shared/ButtonAdd.jsx
+++ 04_movie-search/src/components/shared/ButtonAdd.jsx
@@ -3,7 +3,7 @@ export default function ButtonAdd({ imdbID, updateWatchlist }) {
         <div className='update-watchlist-btn__container'>
           <label className='update-watchlist-btn__label'>Add</label>
           <button className="update-watchlist-btn__btn" onClick={() => updateWatchlist(imdbID)}>
-            <img className='update-watchlist-btn__icon' src="./public/assets/images/icon-add.svg" alt="Add movie to your watchlist" />
+            <img className='update-watchlist-btn__icon' src="/assets/images/icon-add.svg" alt="Add movie to your watchlist" />
           </button>
         </div>
   );
