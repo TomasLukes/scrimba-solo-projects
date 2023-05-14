5107c44204055d027352bdf2a610e1d1b0a07c62
TomasLukes <lukest@seznam.cz>
2023-05-14 11:34:54 +0200

    03/ Fixed correct logo img path

--- 03_triviathon/src/components/Logo.jsx
+++ 03_triviathon/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="flex flex-row items-center gap-2 self-center">
-      <img  className="max-w-100 w-auto" src="./public/images/triviathon-logo-main.png" alt="Triviathon logo" />
+      <img  className="max-w-100 w-auto" src="/images/triviathon-logo-main.png" alt="Triviathon logo" />
       <h2 className="text-3xl font-bold">Triviathon</h2>
     </div>
   )
